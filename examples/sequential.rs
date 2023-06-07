e1788bdf18141c883ed6b108d59f8bb4f3254217
aegroto <aegroto@protonmail.com>
2023-06-07 11:55:56 +0200

    updated sequential example

--- examples/sequential.rs
+++ examples/sequential.rs
@@ -1,11 +1,57 @@
-use remotia::pipeline::Pipeline;
+use std::collections::HashMap;
+
 use remotia::pipeline::component::Component;
+use remotia::pipeline::Pipeline;
 use remotia::processors::containers::sequential::Sequential;
-use remotia::processors::debug::random_dropper::RandomFrameDropper;
-use remotia::processors::frame_drop::threshold::ThresholdBasedFrameDropper;
 use remotia::processors::functional::Function;
 use remotia::processors::ticker::Ticker;
-use remotia::time::add::TimestampAdder;
+use remotia::profilation::frame_drop::threshold::ThresholdBasedFrameDropper;
+use remotia::profilation::time::add::TimestampAdder;
+use remotia::traits::{FrameError, FrameProperties};
+
+#[derive(Debug, Copy, Clone)]
+enum Error {
+    TooHighDelay,
+}
+
+#[derive(Debug)]
+struct FrameData {
+    properties: HashMap<String, u128>,
+    error: Option<Error>,
+}
+
+impl Default for FrameData {
+    fn default() -> Self {
+        Self {
+            properties: Default::default(),
+            error: None
+        }
+    }
+}
+
+impl FrameProperties<u128> for FrameData {
+    fn set(&mut self, key: &str, value: u128) {
+        self.properties.insert(key.to_string(), value);
+    }
+
+    fn get(&mut self, key: &str) -> u128 {
+        *self.properties.get(key).unwrap()
+    }
+}
+
+impl FrameError<Error> for FrameData {
+    fn report_error(&mut self, error: Error) {
+        self.error = Some(error);
+    }
+
+    fn has_error(&mut self) -> bool {
+        self.error.is_some()
+    }
+
+    fn get_error(&mut self) -> Error {
+        self.error.unwrap()
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -20,24 +66,40 @@ async fn main() {
         }));
 
     let delay_check = Sequential::new()
-        .append(ThresholdBasedFrameDropper::new("generation_time", 800))
+        .append(ThresholdBasedFrameDropper::new("generation_time", 800, Error::TooHighDelay))
         .append(Function::new(|frame_data| {
-            println!("[Delay Checker] This frame DTO has passed the check: {:?}", frame_data);
+            println!(
+                "[Delay Checker] This frame DTO has passed the check: {:?}",
+                frame_data
+            );
             Some(frame_data)
         }));
 
     let dropper = Sequential::new()
-        .append(RandomFrameDropper::new(0.5))
         .append(Function::new(|frame_data| {
-            println!("[Dropper] This frame DTO has not been dropped: {:?}", frame_data);
+            use rand::Rng;
+            let mut rng = rand::thread_rng();
+
+            if rng.gen::<f32>() <= 0.5 {
+                None
+            } else {
+                Some(frame_data)
+            }
+        }))
+        .append(Function::new(|frame_data| {
+            println!(
+                "[Dropper] This frame DTO has not been dropped: {:?}",
+                frame_data
+            );
             Some(frame_data)
         }));
 
-    let handles = Pipeline::new()
-        .link(Component::new()
-            .append(generator)
-            .append(dropper)
-            .append(delay_check)
+    let handles = Pipeline::<FrameData>::new()
+        .link(
+            Component::new()
+                .append(generator)
+                .append(dropper)
+                .append(delay_check),
         )
         .run();
 
@@ -45,4 +107,3 @@ async fn main() {
         handle.await.unwrap();
     }
 }
-
